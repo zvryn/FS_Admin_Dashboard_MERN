31271012906ba08abdc5a35bede6eb5fc9aed079
zvryn <sarinkanok@gmail.com>
2024-12-04 13:34:48 +0700

    feat:add product page

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Dashboard from "./scenes/dashboard";
 import Layout from "./scenes/layout";
 import { Navigate } from "react-router-dom";
 import { Routes } from "react-router-dom";
+import Products from "./scenes/products";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -22,6 +23,7 @@ function App() {
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
             </Route>
           </Routes>
         </ThemeProvider>
